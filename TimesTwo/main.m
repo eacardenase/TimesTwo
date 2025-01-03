e9f47b836fe3c11b925fb94f8754f077565759df
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-02 20:45:19 -0500

    Initial Commit

---
+++ TimesTwo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimesTwo
+//
+//  Created by Edwin Cardenas on 2/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
